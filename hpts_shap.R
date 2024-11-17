04b735dede8bc0cb7aef6b9c22e0a279be5284bb
stuartore <stuart.dr@outlook.com>
2024-11-18 00:39:35 +0800

    HPTS: add CVD incident prediction for XGBoost with SHAP value

--- hpts_shap.R
+++ hpts_shap.R
@@ -7,6 +7,8 @@ library(caret)
 library(pROC)
 library(tibble)
 
+# PA患者发生CVD风险事件的机器学习
+
 # 引用文件
 source("hpts_excel.R")
 
@@ -16,9 +18,15 @@ baseline_df <- stu_read_excel(excel_file = "excel/基线数据.xlsx", sheet_list
 # 转换为因子
 # 是否诊断PA、是否诊断原高、性别（男1、女0）、冠心病、脑卒中、糖尿病、房颤、目前是否吸烟、饮酒、ACEIs/ARBs/ARNI、a受体阻滞剂、B受体阻滞剂、利尿剂、钙通道阻滞剂、降脂药、抗凝剂、螺内酯、降糖药
 
+# 增加一列CVD，如果冠心病或脑卒中为1，则CVD为1，否则为0
+baseline_df$CVD <- ifelse(baseline_df$`冠心病` == 1 | baseline_df$`脑卒中` == 1, 1, 0)
+
+# 筛选“是否诊断PA”为1的数据
+baseline_df <- baseline_df[baseline_df$`是否诊断PA` == 1,]
 
 # 转换为因子
 baseline_df$`是否诊断PA` <- as.numeric(baseline_df$`是否诊断PA`)
+baseline_df$CVD <- as.numeric(baseline_df$CVD)
 baseline_df$`是否诊断原高` <- as.factor(baseline_df$`是否诊断原高`)
 baseline_df$`性别（男1、女0）` <- as.factor(baseline_df$`性别（男1、女0）`)
 baseline_df$`冠心病` <- as.factor(baseline_df$`冠心病`)
@@ -39,10 +47,16 @@ baseline_df$`降糖药` <- as.factor(baseline_df$`降糖药`)
 
 
 # 将需要分析的提取出来
-baseline_df_sample <- baseline_df[,c("是否诊断PA", "BMI", "BSA", "AHI指数", "糖化血红蛋白", 
-  "白细胞", "中性粒细胞", "淋巴细胞", "血小板", "AST", "ALT", "甘油三酯", "LDL-C", "载脂蛋白-B", "脂蛋白a", "肌酐", "eGFR", "钾",
-  "卧位醛固酮（5点）", "卧位肾素（5点）", "卧位ARR", "立位醛固酮（7点）", "立位肾素（7点）", "立位ARR", 
-  "血浆皮质醇测定（早上8:00）", "血浆皮质醇测定（下午16:00）", "血浆皮质醇测定（晚上0:00）", "24小时平均收缩压", "24小时平均舒张压", "24小时平均心率",
+baseline_df_sample <- baseline_df[,c("CVD", "BMI", 
+  #"BSA", 
+  "AHI指数", "糖化血红蛋白", 
+  "白细胞", 
+  "中性粒细胞", "淋巴细胞", "血小板", "AST", 
+  "ALT", 
+  "甘油三酯", "LDL-C", "载脂蛋白-B", "脂蛋白a", "肌酐", 
+  #"eGFR", "钾",
+  #"卧位醛固酮（5点）", "卧位肾素（5点）", "卧位ARR", "立位醛固酮（7点）", "立位肾素（7点）", "立位ARR", 
+  #"血浆皮质醇测定（早上8:00）", "血浆皮质醇测定（下午16:00）", "血浆皮质醇测定（晚上0:00）", "24小时平均收缩压", "24小时平均舒张压", "24小时平均心率",
   "主动脉瓣环", "室间隔厚度", "左室射血分数"
 )]
 
@@ -52,9 +66,6 @@ baseline_df_sample <- na.omit(baseline_df_sample)
 f1<-function(x){
   return((x-min(x)) / (max(x)-min(x)))
 }
-
-#baseline_df_sample.scale <- as.data.frame(lapply(baseline_df_sample[,c(2:15)], f1))
-#baseline_df_sample.scale <- cbind(`是否诊断PA`=baseline_df_sample$`是否诊断PA`, baseline_df_sample.scale)
 
 # 分成建模和验证组
 set.seed(1234)
@@ -69,7 +80,7 @@ baseline_df_sample_test<- baseline_df_sample[-inTrain,]#30%数据集
 # XGboost模型
 model_xgboost = xgboost(
   data = as.matrix(baseline_df_sample_train[,c(2:15)]),#训练集的自变量矩阵
-                   label = baseline_df_sample_train$`是否诊断PA`,
+                   label = baseline_df_sample_train$`CVD`,
                    max_depth = 3, 
                    eta = 1, 
                    nthread = 2, 
@@ -81,14 +92,18 @@ model_xgboost = xgboost(
 #pred_svm <- predict(svm, baseline_df_sample.scale, type = "prob")
 pred_xgboost <- predict(model_xgboost, as.matrix(baseline_df_sample_test[,c(2:15)]), type = "prob")
 
+# 绘制ROC曲线
+roc_obj <- roc(response = baseline_df_sample_test$`CVD`, predictor = pred_xgboost)
+plot(roc_obj, main = "ROC Curve CVD Predict for XGBoost Model")
+abline(a = 0, b = 1, lty = 2, col = "gray")  # 添加对角线
 
-# 计算AUC
-#auc_svm <- roc(baseline_df_sample.scale$`是否诊断PA`, pred_svm[,2])$auc
-auc_xgboost <- roc(baseline_df_sample_test$`是否诊断PA`, pred_xgboost)$auc
+# 计算AUC值
+auc_value <- auc(roc_obj)
+text(x = 0.5, y = 0.25, labels = paste("AUC =", round(auc_value, 4)), cex = 1.2, col = "#0077ff")
 
 # 计算SHAP值
 shap_values <- shap.values(model_xgboost, as.matrix(baseline_df_sample_train[,c(2:15)]))
 
 # To prepare the long-format data:
 shap_long <- shap.prep(xgb_model = model_xgboost, X_train = as.matrix(baseline_df_sample_train[,c(2:15)]),)
-shap.plot.summary(shap_long)
+#shap.plot.summary(shap_long)
