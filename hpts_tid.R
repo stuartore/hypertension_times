1c3de2f4852ea6216d37025b78c9d724b230ab42
stuartore <stuart.dr@outlook.com>
2024-12-18 01:31:34 +0800

    HPTS: 针对LVMI判断的心脏靶器官损害区分了性别

--- hpts_tid.R
+++ hpts_tid.R
@@ -3,7 +3,7 @@ source("./nanshan_indexes.R")
 
 library(openxlsx)
 library(dplyr)
-library(tableone)
+library(compareGroups)
 library(showtext)
 
 # 图片显示中文
@@ -25,7 +25,7 @@ DataColStr2Num <- function(data, columns=c()) {
   return(data)
 }
 
-data <- read.xlsx("./excel/数据内容1.xlsx", sheet = 1)
+data <- read.xlsx("./excel/数据内容.xlsx", sheet = 1)
 
 # 假设您的数据框名为data
 # 将“血糖”从mmol/L转换为mg/dL
@@ -43,7 +43,7 @@ data$UHR <- UHR(data$`总胆固醇`, data$`高密度脂蛋白胆固醇`)
 
 data <- data %>%
   mutate(
-    `颈动脉靶器官损害` = ifelse(`左侧内中膜` > 0.9 | grepl("斑块", `颈动脉彩超(颈总颈内颈外椎动脉锁骨下)+图文报告诊断意见`),
+    `颈动脉靶器官损害` = ifelse(`左侧内中膜` > 0.9 | `右侧内中膜` > 0.9 | grepl("斑块", `颈动脉彩超(颈总颈内颈外椎动脉锁骨下)+图文报告诊断意见`),
         1,
         0
     )
@@ -52,9 +52,21 @@ data <- data %>%
 data <- data %>%
   mutate(
     `心脏靶器官损害` = ifelse(
-      LVMI > 120,
+      LVMI > 115,
       1,
       0
+    )
+  )
+
+# 临时根据Sex_Hb变量进行分组
+data <- data %>%
+  mutate(
+    # LVMI 男大于115，女大于95，默认大于95
+    `心脏靶器官损害` = ifelse(
+      Sex_Hb == "男",
+      ifelse(LVMI > 115, 1, 0),
+      # 默认NA和女大于95为1
+      ifelse(LVMI > 95, 1, 0)
     )
   )
 
@@ -68,23 +80,90 @@ ultrasound_list <- c("肾动脉水平腹主动脉PSV", "左肾动脉主干PSV", 
 blood_pressure_list <- c("24小时收缩压平均值", "24小时舒张压平均值", "白天收缩压平均值", "白天舒张压平均值", "夜间收缩压平均值", "夜间舒张压平均值")
 other_list <- c("尿微量白蛋白", "肌酐(尿)", "尿微量白蛋白/肌酐比值")
 
-indexes_list <- c("BMI", "BRI", "LVMI", "HGI", "PHR", "METS_IR", "AIP", "UHR")
+indexes_list <- c(
+  #"BMI", "BRI",
+  "LVMI", "HGI","PHR", 
+  # "METS_IR", 
+  "AIP", "UHR"
+)
+
+# 获取LVMI的四分位数切点
+lvmi_quantiles <- quantile(data$LVMI, probs = c(0.25, 0.5, 0.75), na.rm = TRUE)
+
+# 创建一个新的变量用于存储分组标签
+data$`LVMI Group` <- cut(data$LVMI, 
+                       breaks = c(-Inf, lvmi_quantiles, Inf), 
+                       labels = c("Q1", "Q2", "Q3", "Q4"), 
+                       include.lowest = TRUE)
 
 # 绘制table one
 tableone_vars <- c(blood_list, ultrasound_list, blood_pressure_list, other_list, indexes_list)
-the_table1 <- CreateTableOne(vars = tableone_vars, 
-  strata = "颈动脉靶器官损害", 
-  data = data)
+# 去除数据不足变量
+tableone_vars <- setdiff(tableone_vars, "超敏C反应蛋白测定（全程）")
 
-the_table1_1 <- CreateTableOne(vars = tableone_vars, 
-  strata = "心脏靶器官损害", 
-  data = data)
+tableone_data <- data[, c(blood_list, ultrasound_list, blood_pressure_list, c("LVMI Group"), indexes_list, c("颈动脉靶器官损害", "心脏靶器官损害"))] %>%
+  select(-`超敏C反应蛋白测定（全程）`) #%>%
+#  select(-`LVMI`)
+
+# 将颈动脉内膜损害、心脏靶器官损害转换为factor因子
+tableone_data$`颈动脉靶器官损害` <- factor(tableone_data$`颈动脉靶器官损害`, levels = c(0, 1), labels = c("未发生颈动脉内膜损害", "颈动脉内膜损害"))
+tableone_data$`心脏靶器官损害` <- factor(tableone_data$`心脏靶器官损害`, levels = c(0, 1), labels = c("未发生心脏靶器官损害", "心脏靶器官损害"))
+
+# 颈动脉靶器官损害、心脏靶器官损害
+tableone_tid <- descrTable(
+  `颈动脉靶器官损害` ~ .
+    , data = tableone_data,
+    sd.type = 3, hide.no = "no", include.label = FALSE
+)
+
+tableone_heart <- descrTable(
+  `心脏靶器官损害` ~ .
+    , data = tableone_data,
+    sd.type = 3, hide.no = "no", include.label = FALSE
+)
+
+# export2word导出
+export2word(tableone_tid, "颈动脉内膜基线.docx")
+export2word(tableone_heart, "心脏靶器官基线.docx")
+
 
 library(ggplot2)
 
+# 颈动脉内膜平均厚度
+data <- data %>%
+  mutate(
+    # 左侧右侧/2
+    `颈动脉内膜厚度` = (`左侧内中膜` + `右侧内中膜`) / 2
+  )
+
 # 循环针对tableone_vars所有变量，拟合平滑曲线
-p <- ggplot(data, aes(x = UHR, y = `LVMI`)) +
+p <- ggplot(data, aes(x = LVMI, y = `颈动脉内膜厚度`)) +
   geom_point() +  # 添加散点图
   geom_smooth(method = "lm", formula = y ~ x, color = "blue")  # 添加平滑曲线
 
 print(p)
+
+# 加载必要的包
+library(rms)
+library(ggplot2)
+
+# 假设你的数据存储在名为data的数据框中，先准备数据分布对象（这里要替换为你的实际数据框和变量名）
+dd <- datadist(data[, c("LVMI", "颈动脉靶器官损害")])
+options(datadist = 'dd')
+
+# 使用rcs()函数创建受限三次样条，这里同样以4个节点为例（节点数量可根据实际情况调整）
+# 拟合逻辑回归模型，以颈动脉靶器官损害为响应变量，LVMI为自变量且应用受限三次样条变换
+model <- lrm(`颈动脉靶器官损害` ~ rcs(LVMI, 4), data = data)
+
+# 检查模型摘要
+summary(model)
+
+# 使用Predict()函数预测概率
+pred <- Predict(model, LVMI, fun = plogis, ref.zero = TRUE)
+
+# 使用ggplot2绘制RCS图
+ggplot(data = pred, aes(x = LVMI, y = pred)) +
+  geom_line(color = "blue") +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha = 0.2) +
+  labs(title = "RCS for LVMI and Carotid Target Organ Damage", x = "LVMI", y = "Predicted Probability") +
+  theme_minimal()
